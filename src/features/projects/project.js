4396ae49831a62f47ae3befd00f2e8f8a906bfb7
JerrardJ <jerrard1040@gmail.com>
2026-04-30 14:41:06 +0700

    [Fix] Description format updated on projects.jsx

--- src/features/projects/project.js
+++ src/features/projects/project.js
@@ -7,6 +7,10 @@ export const project = [
   {
     title: "Eco-Enzyme Quality Monitoring System",
     description:
+      "Client : Thesis Project<br/>" +
+      "Source : Bina Nusantara @Bandung<br/>" +
+      "Type : Team Project - 2 Person<br/>" +
+      "Role : Front End Developer<br/><br/>" +
       "This project uses IoT devices to collect measurement values from an eco-enzyme box. Data is sent to a database and shown through a website dashboard. The system monitors temperature, humidity, gas levels, turbidity, pH, and salt content. The website can notify users about the eco-enzyme condition and indicate whether the liquid is ready for use.",
     image: ecoenzyme,
     tags: ["php", "css", "c++", "mysql"],
@@ -15,6 +19,10 @@ export const project = [
   {
     title: "Dapur Ambu Kuliner",
     description:
+      "Client : Dapur Ambu Kuliner<br/>" +
+      "Source : Community Development at Bina Nusantara @Bandung<br/>" +
+      "Type : Team Project - 2 Person<br/>" +
+      "Role : Front End Developer<br/><br/>" +
       "Dapur Ambu Kuliner is a web application and digital platform dedicated to celebrating, documenting, and promoting traditional culinary heritage. Focusing on authentic home-cooked meals and family heirloom recipes, this project aims to bring the warmth and comfort of `mother's cooking` into a modern digital format that is easily accessible to food enthusiasts everywhere.",
     image: ambu,
     tags: ["php", "css", "javascript", "mysql"],
@@ -23,6 +31,10 @@ export const project = [
   {
     title: "Smart Lamp",
     description:
+      "Client : Human Computer Interaction Assignment<br/>" +
+      "Source : Bina Nusantara @Bandung<br/>" +
+      "Type : Team Project - 4 Person<br/>" +
+      "Role : Wireframe & Mock Up Design & Front End Developer<br/><br/>" +
       "This web-based Smart Lamp application allows users to control the lighting of lamps both from close range and remotely through devices connected to the internet, such as computers, smartphones, or tablets. This Smart Lamp application has timer features and the ability to add devices. This advanced web application for the Smart Lamp allows users to seamlessly control their lighting from close range and remotely.",
     image: lamp,
     tags: ["html", "css", "javascript"],
@@ -31,6 +43,10 @@ export const project = [
   {
     title: "Pokemon Dex",
     description:
+      "Client : Mobile Programming Assignment<br/>" +
+      "Source : Bina Nusantara @Bandung<br/>" +
+      "Type : Team Project - 4 Person<br/>" +
+      "Role : Wireframe & Front End Mobile Programming<br/><br/>" +
       "Podéx is a comprehensive offline database application specifically designed for Pokémon Generation 1. It provides detailed base stats, complete evolution charts, type matchups, and accurate movesets without requiring an internet connection. Built for nostalgic emulator users, this handy utility guarantees a highly reliable, quick, and completely ad-free reference guide to enjoy anytime, anywhere.",
     image: podex,
     tags: ["java"],
@@ -39,6 +55,10 @@ export const project = [
   {
     title: "Bank Indonesia Augmented Reality",
     description:
+      "Client : Augmented Reality Assignment<br/>" +
+      "Source : Bina Nusantara @Bandung<br/>" +
+      "Type : Team Project - 4 Person<br/>" +
+      "Role : Wireframe & Front End Mobile Programming<br/><br/>" +
       "This innovative Augmented Reality application provides an immersive experience by displaying a detailed digital projection of the entire Bank Indonesia building located in Bandung. Users can visually explore its magnificent architecture while discovering a unique, captivating fun fact that highlights the rich cultural heritage and profound significance of this iconic historical city landmark.",
     image: bank,
     tags: ["unity", "java", "blender"],
