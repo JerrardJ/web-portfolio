783ec84e7f3d9f0bc764d28418bea756e33cab38
JerrardJ <jerrard1040@gmail.com>
2026-04-30 15:47:13 +0700

    [Fix] Description format updated on achievementData.js

--- src/features/achievement/achievementData.js
+++ src/features/achievement/achievementData.js
@@ -5,14 +5,26 @@ export const achievement = [
   {
     title: "Digital Commerce Disruption: TikTok Shop and Its Influence on MSMEs in Urban Indonesia",
     description:
-      "We pleased to share that our paper, ‘Digital Commerce Disruption: TikTok Shop and Its Influence on MSMEs in Urban Indonesia,’ was published with an IEEE publisher and presented on 22-23 August 2025. The paper was presented online via Zoom at the 2025 World Skills Conference on Universal Data Analytics and Sciences (WorldSUAS), an international conference hosted in Indore, India.",
-    image: ieee1,
+      "Event Name : 2025 World Skills Conference on Universal Data Analytics and Sciences (WorldSUAS)<br/><br/>" +
+      "Publisher : IEEE<br/>" +
+      "DOI : 10.1109/WorldSUAS66815.2025.11199293<br/>" +
+      "Date : 22-23 August 2025<br/>" +
+      "Location : Indore, India (Online via Zoom)<br/>" +
+      "Role : Writer<br/><br/>" +
+      "Tiktok is a social media application for uploading short-duration videos and is growing rapidly, in 2020 the total downloads of the tiktok application have reached 63.3 million times and the Indonesian state participates by 11%, reaching many demographics of people. Tiktok also has visual effects, editing, and audio features that are easy to use by various groups, especially teenagers. The platform also has opportunities for people to express themselves, as a place for entertainment, education and a place for people to express themselves through existing content.",
+      image: ieee1,
     url: "https://ieeexplore.ieee.org/abstract/document/11199293/",
   },
   {
     title: "Determinants of Coffee Shop Preference Through Online Review Data Analytics",
     description:
-      "We pleased to share that our paper, ‘Determinants of Coffee Shop Preference Through Online Review Data Analytics,’ was published with an IEEE publisher on September 24, 2025. This paper was presented on August 15–16, 2025 at the 2025 4th International Conference on Creative Communication and Innovative Technology (ICCIT), a national conference held in Cirebon, Indonesia.",
+      "Event Name : 4th International Conference on Creative Communication and Innovative Technology (ICCIT)<br/><br/>" +
+      "Publisher : IEEE<br/>" +
+      "DOI : 10.1109/ICCIT65724.2025.11166970<br/>" +
+      "Date : August 15–16, 2025<br/>" +
+      "Location : Cirebon, Indonesia (Online via Zoom)<br/>" +
+      "Role : Paper Researcher & Writer<br/><br/>" +
+      "The rapid expansion of the coffee shop industry, driven by digital consumerism, necessitates a deeper understanding of the factors influencing customer preference for business sustainability. This study aims to identify the primary determinants of customer choice by analyzing online review data. Focusing on Kanoko Coffee Shop in Bandung, Indonesia as a representative Micro, Small, and Medium-sized Enterprise (MSME), this research applies a data analytics methodology to 289 user-generated reviews collected from various online platforms between 2019 and 2024.",
     image: ieee2,
     url: "https://ieeexplore.ieee.org/abstract/document/11166970",
   },
