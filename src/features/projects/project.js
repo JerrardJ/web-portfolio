8057cc72c78065624ec51f8b9bb909487c938646
JerrardJ <jerrard1040@gmail.com>
2026-05-02 15:52:15 +0700

    [Fix] Description format has been fixed

--- src/features/projects/project.js
+++ src/features/projects/project.js
@@ -1,26 +1,29 @@
 import podex from "../../assets/images/podex.png";
 import lamp from "../../assets/images/lamp.png";
 import ecoenzyme from "../../assets/images/ecoenzyme.png";
+import storage from "../../assets/images/storage.png";
 import ambu from "../../assets/images/ambu.png";
+import car from "../../assets/images/car.png";
 import bank from "../../assets/images/bank.png";
+
 export const project = [
   {
     title: "Eco-Enzyme Quality Monitoring System",
     description:
       "Client : Thesis Project<br/>" +
-      "Source : Bina Nusantara @Bandung<br/>" +
+      "Source : Bina Nusantara @Bandung (2025)<br/>" +
       "Type : Team Project - 2 Person<br/>" +
       "Role : Front End Developer<br/><br/>" +
       "This project uses IoT devices to collect measurement values from an eco-enzyme box. Data is sent to a database and shown through a website dashboard. The system monitors temperature, humidity, gas levels, turbidity, pH, and salt content. The website can notify users about the eco-enzyme condition and indicate whether the liquid is ready for use.",
     image: ecoenzyme,
-    tags: ["php", "css", "c++", "mysql"],
+    tags: ["php", "html", "css", "javascript", "c++", "mysql", "firebase"],
     sourceCode: "https://github.com/JerrardJ/eco-enzyme-quality-monitoring-system",
   },
   {
     title: "Dapur Ambu Kuliner",
     description:
       "Client : Dapur Ambu Kuliner<br/>" +
-      "Source : Community Development at Bina Nusantara @Bandung<br/>" +
+      "Source : Community Development at Bina Nusantara @Bandung (2024)<br/>" +
       "Type : Team Project - 2 Person<br/>" +
       "Role : Front End Developer<br/><br/>" +
       "This website represents a professional digital platform for a culinary MSME named Dapur Ambu, designed to promote traditional Indonesian cuisine through a clean and structured web interface. The website presents key business information, featured dishes, and contact details in a visually consistent red-themed layout that reflects culinary branding and identity. The homepage introduces Dapur Ambu with a brief business profile, highlighting its focus on authentic local dishes.",
@@ -29,24 +32,36 @@ export const project = [
     sourceCode: "https://github.com/JerrardJ/dapur-ambu-kuliner",
   },
   {
-    title: "Smart Lamp",
+    title: "Storage Item Identificator",
     description:
-      "Client : Human Computer Interaction Assignment<br/>" +
-      "Source : Bina Nusantara @Bandung<br/>" +
+      "Client : Computer Vision in IoT Final Project<br/>" +
+      "Source : Bina Nusantara @Bandung (2023)<br/>" +
       "Type : Team Project - 4 Person<br/>" +
-      "Role : Wireframe & Mock Up Design & Front End Developer<br/><br/>" +
-      "This web-based Smart Lamp application allows users to control the lighting of lamps both from close range and remotely through devices connected to the internet, such as computers, smartphones, or tablets. This Smart Lamp application has timer features and the ability to add devices. This advanced web application for the Smart Lamp allows users to seamlessly control their lighting from close range and remotely.",
-    image: lamp,
-    tags: ["html", "css", "javascript"],
-    sourceCode: "https://github.com/JerrardJ/SmartLamp",
+      "Role : AI Engineer<br/><br/>" +
+      "The general working method in the process of sorting goods using this technology is to identify and classify goods based on certain criteria. This technology not only improves the performance of the sorting accuracy process but also increases efficiency, thereby reducing human labor. Our project is inspired by the self-checkout system.",
+    image: storage,
+    tags: ["YOLOv8", "YOLOv7", "YOLOv6", "YOLOv5", "Jupyter"],
+    sourceCode: "https://github.com/JerrardJ/storage-item-identificator",
+  },
+  {
+    title: "O'Rented",
+    description:
+      "Client : Software Engineering Final Project<br/>" +
+      "Source : Bina Nusantara @Bandung (2022)<br/>" +
+      "Type : Team Project - 5 Person<br/>" +
+      "Role : Wireframe & Front End Mobile Developer<br/><br/>" +
+      "This versatile application connects renters, vehicle owners, and professional drivers in one unified platform. Users can easily rent vehicles tailored to their specific needs or hire a driver if they cannot drive. Additionally, owners of idle cars can list their vehicles for rent, while available drivers can offer their driving services to those in need.",
+    image: car,
+    tags: ["java", "sqlite"],
+    sourceCode: "https://github.com/vtsMwlyn/orented-android",
   },
   {
     title: "Pokemon Dex",
     description:
-      "Client : Mobile Programming Assignment<br/>" +
-      "Source : Bina Nusantara @Bandung<br/>" +
-      "Type : Team Project - 4 Person<br/>" +
-      "Role : Wireframe & Front End Mobile Programming<br/><br/>" +
+      "Client : Mobile Programming Final Project<br/>" +
+      "Source : Bina Nusantara @Bandung (2022)<br/>" +
+      "Type : Team Project - 5 Person<br/>" +
+      "Role : Wireframe & Front End Mobile Developer<br/><br/>" +
       "Podéx is a comprehensive offline database application specifically designed for Pokémon Generation 1. It provides detailed base stats, complete evolution charts, type matchups, and accurate movesets without requiring an internet connection. Built for nostalgic emulator users, this handy utility guarantees a highly reliable, quick, and completely ad-free reference guide to enjoy anytime, anywhere.",
     image: podex,
     tags: ["java"],
@@ -55,13 +70,25 @@ export const project = [
   {
     title: "Bank Indonesia Augmented Reality",
     description:
-      "Client : Augmented Reality Assignment<br/>" +
-      "Source : Bina Nusantara @Bandung<br/>" +
+      "Client : Multimedia & Mixed Reality Final Project<br/>" +
+      "Source : Bina Nusantara @Bandung (2022)<br/>" +
       "Type : Team Project - 4 Person<br/>" +
-      "Role : Wireframe & Front End Mobile Programming<br/><br/>" +
+      "Role : Wireframe & Front End Mobile Developer<br/><br/>" +
       "This innovative Augmented Reality application provides an immersive experience by displaying a detailed digital projection of the entire Bank Indonesia building located in Bandung. Users can visually explore its magnificent architecture while discovering a unique, captivating fun fact that highlights the rich cultural heritage and profound significance of this iconic historical city landmark.",
     image: bank,
     tags: ["unity", "java", "blender"],
     sourceCode: "https://github.com/JerrardJ/BankIndonesiaAR",
+  },
+  {
+    title: "Smart Lamp",
+    description:
+      "Client : Human & Computer Interaction Final Project<br/>" +
+      "Source : Bina Nusantara @Bandung (2021)<br/>" +
+      "Type : Team Project - 4 Person<br/>" +
+      "Role : Wireframe & Mock Up Design & Front End Developer<br/><br/>" +
+      "This web-based Smart Lamp application allows users to control the lighting of lamps both from close range and remotely through devices connected to the internet, such as computers, smartphones, or tablets. This Smart Lamp application has timer features and the ability to add devices. This advanced web application for the Smart Lamp allows users to seamlessly control their lighting from close range and remotely.",
+    image: lamp,
+    tags: ["html", "css", "javascript"],
+    sourceCode: "https://github.com/JerrardJ/SmartLamp",
   },
 ];
